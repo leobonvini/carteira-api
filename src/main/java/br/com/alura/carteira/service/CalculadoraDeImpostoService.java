d64fa2e9240f8256d092e483b1a659cc5f98f3e7
leobonvini <leonardobonvini@id.uff.br>
2021-10-17 17:00:28 -0300

    criacao do service de calculo de imposto e seus testes

---
+++ src/main/java/br/com/alura/carteira/service/CalculadoraDeImpostoService.java
@@ -0,0 +1,26 @@
+package br.com.alura.carteira.service;
+
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+
+import br.com.alura.carteira.modelo.TipoTransacao;
+import br.com.alura.carteira.modelo.Transacao;
+
+public class CalculadoraDeImpostoService {
+	
+	public BigDecimal calcular(Transacao transacao) {
+		if (transacao.getTipo() == TipoTransacao.COMPRA) {
+			return BigDecimal.ZERO;
+		}
+		
+		BigDecimal valorTransacao = 
+				transacao.getPreco().multiply(new BigDecimal(transacao.getQuantidade()));
+		if (valorTransacao.compareTo(new BigDecimal(20000)) < 0) {
+			return BigDecimal.ZERO;
+		}
+		
+		return valorTransacao.multiply(new BigDecimal("0.15"))
+				.setScale(2, RoundingMode.HALF_UP);
+	}
+
+}
